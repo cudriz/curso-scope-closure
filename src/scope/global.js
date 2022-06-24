81ea0d64767d7e21b4d0ccafcbbf02d7c0b342c7
cudriz <carloscudriz21@gmail.com>
2022-06-24 12:04:11 -0500

    1 commit

---
+++ src/scope/global.js
@@ -0,0 +1,15 @@
+var a; //declarando
+var b='b'; //declaramos / asignamos
+b='bb'; //reasignacion
+var a='aa'; // redeclaracion
+
+var fruit='Apple';
+console.log(fruit);
+
+function bestFruit(){console.log(fruit);
+}
+bestFruit();
+function countries(){country='Colombia';
+console.log(country);
+}
+countries();console.log(country);
